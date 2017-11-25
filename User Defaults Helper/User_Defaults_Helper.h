cd91c5f116dfc6615ae1f38134e81a2711a04770
Eric Boxer <eboxhead@gmail.com>
2017-11-25 10:07:19 -0500

    Initial Commit

---
+++ User Defaults Helper/User_Defaults_Helper.h
@@ -0,0 +1,19 @@
+//
+//  User_Defaults_Helper.h
+//  User Defaults Helper
+//
+//  Created by Eric Boxer on 11/25/17.
+//  Copyright © 2017 Eric Boxer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for User_Defaults_Helper.
+FOUNDATION_EXPORT double User_Defaults_HelperVersionNumber;
+
+//! Project version string for User_Defaults_Helper.
+FOUNDATION_EXPORT const unsigned char User_Defaults_HelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <User_Defaults_Helper/PublicHeader.h>
+
+
---
+++ User Defaults HelperTests/User_Defaults_HelperTests.swift
@@ -0,0 +1,36 @@
+//
+//  User_Defaults_HelperTests.swift
+//  User Defaults HelperTests
+//
+//  Created by Eric Boxer on 11/25/17.
+//  Copyright © 2017 Eric Boxer. All rights reserved.
+//
+
+import XCTest
+@testable import User_Defaults_Helper
+
+class User_Defaults_HelperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
